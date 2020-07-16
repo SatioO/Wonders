aef5321268ebb3451ba0db804c5f6d5850372ebc
SatioO <vaibhav.satam29991@gmail.com>
2020-07-16 19:38:27 +0530

    animated

--- lib/ui/pages/animations.dart
+++ lib/ui/pages/animations.dart
@@ -1,61 +1,85 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
+
+class AnimatedCircle extends AnimatedWidget {
+  final Tween<double> tween;
+  final Animation<double> animation;
+  final double radius;
+
+  AnimatedCircle(
+      {Key key,
+      @required this.animation,
+      @required this.tween,
+      @required this.radius})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+        alignment: FractionalOffset.centerLeft,
+        transform: Matrix4.identity()..scale(tween.evaluate(animation)),
+        child: Container(
+          height: radius,
+          width: radius,
+          decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.all(Radius.circular(radius / 2))),
+        ));
+  }
+}
 
 class Animations extends StatefulWidget {
   @override
   _AnimationsState createState() => _AnimationsState();
 }
 
-class _AnimationsState extends State<Animations>
-    with SingleTickerProviderStateMixin {
-  final double radius = 80.0;
-  AnimationController _controller;
-  Animation<double> _rotateY;
-  Animation<double> _scale;
+class _AnimationsState extends State<Animations> with TickerProviderStateMixin {
+  final double radius = 88.0;
+  AnimationController animationController;
+  Animation startAnimation;
+  Animation scale;
 
   @override
   void initState() {
     super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 750));
 
-    _controller =
-        AnimationController(vsync: this, duration: Duration(seconds: 1));
-    Animation<double> curve =
-        CurvedAnimation(parent: _controller, curve: Curves.ease);
-    _rotateY = Tween<double>(end: 1, begin: 0).animate(curve);
-    _scale = Tween<double>(end: 2, begin: 1).animate(CurvedAnimation(
-        parent: _controller, curve: Interval(0.1, 0.6, curve: Curves.ease)));
-  }
-
-  Widget _buildAnimation(BuildContext context, Widget child) {
-    return Stack(
-      children: <Widget>[
-        Transform(
-            alignment: FractionalOffset.center,
-            transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.002)
-              ..rotateY(math.pi * _rotateY.value)
-              ..scale(_scale.value),
-            child: GestureDetector(
-                onTap: () => _controller.forward(), child: child))
-      ],
+    startAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.000, 0.500, curve: Curves.easeInExpo),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        body: SafeArea(
-            child: Center(
-                child: AnimatedBuilder(
-      animation: _rotateY,
-      builder: _buildAnimation,
-      child: Container(
-        height: radius,
-        width: radius,
-        decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.all(Radius.circular(radius / 2))),
+        body: Stack(children: [
+      Container(
+        color: Colors.grey.shade700,
+        width: screenWidth,
+        height: double.infinity,
       ),
-    ))));
+      Transform(
+          transform: Matrix4.identity()
+            ..translate(
+              screenWidth / 2 - radius / 2.0,
+              screenHeight - radius - 80,
+            ),
+          child: GestureDetector(
+            onTap: () => animationController.forward(),
+            child: Stack(
+              children: <Widget>[
+                AnimatedCircle(
+                  animation: startAnimation,
+                  radius: radius,
+                  tween: Tween(begin: 1.0, end: radius),
+                )
+              ],
+            ),
+          ))
+    ]));
   }
 }
