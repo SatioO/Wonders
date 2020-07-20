96363527cb6328efddf14037246e836ecbc21264
SatioO <vaibhav.satam29991@gmail.com>
2020-07-21 02:10:16 +0530

    added title

--- lib/ui/pages/bottomsheet.dart
+++ lib/ui/pages/bottomsheet.dart
@@ -1,5 +1,6 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 class CustomBottomSheet extends StatefulWidget {
   @override
@@ -27,6 +28,12 @@ class _SheetState extends State<Sheet> with SingleTickerProviderStateMixin {
 
   double minHeight = 120.0;
   double get maxHeight => MediaQuery.of(context).size.height;
+  double get headerTopMargin =>
+      lerp(40, 10 + MediaQuery.of(context).padding.top);
+  double get headerFontSize => lerp(18, 24);
+
+  double lerp(double min, double max) =>
+      lerpDouble(min, max, _controller.value);
 
   @override
   void initState() {
@@ -82,10 +89,60 @@ class _SheetState extends State<Sheet> with SingleTickerProviderStateMixin {
                 color: Color(0xFF162A49),
                 borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
               ),
+              child: Stack(
+                children: <Widget>[
+                  MenuButton(),
+                  SheetHeader(
+                    //<-- Add a header with params
+                    fontSize: headerFontSize,
+                    topMargin: headerTopMargin,
+                  ),
+                ],
+              ),
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      //<-- Align the icon to bottom right corner
+      right: 0,
+      bottom: 24,
+      child: Icon(
+        Icons.menu,
+        color: Colors.white,
+        size: 28,
+      ),
+    );
+  }
+}
+
+class SheetHeader extends StatelessWidget {
+  final double fontSize;
+  final double topMargin;
+
+  const SheetHeader(
+      {Key key, @required this.fontSize, @required this.topMargin})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: topMargin,
+      child: Text(
+        'Booked Exhibition',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: fontSize,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
     );
   }
 }
